3f57547a5276fde2581fd67a1eb7b01ed6929dac
Joao Vitor <jvads2005@gmail.com>
2025-04-23 09:13:55 -0300

    feat: implement user-service domain

---
+++ BackEnd/UserService/src/UserService.Domain/Interfaces/IRefreshTokenRepository.cs
@@ -0,0 +1,10 @@
+namespace UserService.Domain.Interfaces;
+
+public interface IRefreshTokenRepository
+{
+    Task<RefreshToken> CreateAsync(RefreshToken refreshToken);
+    Task<RefreshToken?> GetTokenAsync(string refreshToken);
+    Task<RefreshToken?> GetUserTokenAsync(Guid userId);
+    Task RevokeTokenAsync(RefreshToken refreshToken);
+    Task DeleteAllTokens();
+}
